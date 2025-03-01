34fe301939195d7b4ea97370c0fe2fb2d8e87be3
UCardini <smit3cg@mail.uc.edu>
2025-02-28 19:28:31 -0500

    Base configuration. Any added after will be noted.

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,15 @@
 require "nvchad.options"
 
--- add yours here!
-
 local o = vim.o
-o.cursorlineopt ='both' -- to enable cursorline!
+o.expandtab = true
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+
+o.ignorecase = true
+o.smartcase = true
+
+--o.breakindent = true
+
+o.completeopt = "menuone,noselect"
+o.cursorlineopt = "both" -- to enable cursorline!
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,39 +1,93 @@
 return {
-  {
-    "vimwiki/vimwiki",
-    lazy = false,
-  },
-  {
-    "lervag/vimtex",
-    lazy = false,
-    init = function()
-      vim.g.vimtex_compiler = {
-        build_dir = 'build',
-      }
-    end,
-  },
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+    {
+        "lewis6991/hover.nvim",
+        config = function()
+            require("hover").setup {
+                init = function()
+                    -- Require providers
+                    require "hover.providers.lsp"
+                    -- require('hover.providers.gh')
+                    -- require('hover.providers.gh_user')
+                    -- require('hover.providers.jira')
+                    -- require('hover.providers.dap')
+                    -- require('hover.providers.fold_preview')
+                    -- require('hover.providers.diagnostic')
+                    -- require('hover.providers.man')
+                    -- require('hover.providers.dictionary')
+                end,
+                preview_opts = {
+                    border = "single",
+                },
+                -- Whether the contents of a currently open hover window should be moved
+                -- to a :h preview-window when pressing the hover keymap.
+                preview_window = false,
+                title = true,
+                mouse_providers = {
+                    "LSP",
+                },
+                mouse_delay = 1000,
+            }
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+            -- Setup keymaps
+            vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
+            vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
+            vim.keymap.set("n", "<C-p>", function()
+                require("hover").hover_switch "previous"
+            end, { desc = "hover.nvim (previous source)" })
+            vim.keymap.set("n", "<C-n>", function()
+                require("hover").hover_switch "next"
+            end, { desc = "hover.nvim (next source)" })
 
-  {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css", "markdown",
-        "cpp", "latex", "python"
-  		},
-  	},
-  },
+            -- Mouse support
+            vim.keymap.set("n", "<MouseMove>", require("hover").hover_mouse, { desc = "hover.nvim (mouse)" })
+            vim.o.mousemoveevent = true
+        end,
+    },
+    {
+        "rcarriga/nvim-notify", -- Notifications
+    },
+    {
+        "vimwiki/vimwiki",
+        lazy = false, -- Load Vimwiki at startup
+        init = function()
+            -- Vimwiki configuration
+            vim.g.vimwiki_list = {
+                {
+                    path = "~/Documents/", -- Path to your wiki
+                    syntax = "markdown", -- Use Markdown syntax
+                    ext = ".md", -- Use .md file extension
+                },
+            }
+        end,
+    },
+
+    {
+        "stevearc/conform.nvim",
+        -- event = 'BufWritePre', -- uncomment for format on save
+        opts = require "configs.conform",
+    },
+
+    -- These are some examples, uncomment them if you want to see them work!
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            require "configs.lspconfig"
+        end,
+    },
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "vim",
+                "lua",
+                "vimdoc",
+                "html",
+                "css",
+                "markdown",
+                "cpp",
+                "python",
+            },
+        },
+    },
 }
